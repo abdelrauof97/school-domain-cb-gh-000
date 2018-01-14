2708787d150d69eb408b93f25eee7d0cd8d3c0f9
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-14 18:54:21 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,22 @@
-# code here!
+class School 
+  
+  attr_reader :name, :roster
+
+  def initialize(school_name)
+    @school_name = school_name
+    @roster = {}
+  end
+
+  def add_student=(student_name,student_grade)
+    @student_name = student_name
+    @student_grade = student_grade
+  end
+  
+  def add_student
+    @roster[@student_grade] = []
+    @roster[@student_grade] << @student_name
+  end
+  
+end 
+
+# learn --fail-fast
