83512b5a6178cd7ab2ebfb26fdc5a15ee92bc570
abdelrauof mohammed <abdelrauof97@gmail.com>
2018-01-14 19:24:57 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,9 +8,9 @@ class School
   end
 
   def add_student(student_name,student_grade)
-    @student_name = student_name
-    @student_grade = student_grade
-    
+           
+    @roster[student_grade] = []
+    @roster[student_grade] << student_name
   end
   
   def add_student
